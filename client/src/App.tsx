a9eda77fbd760a71fdc31430fbde421d2e3886ad
sapar6ek0v <razetka0309@gmail.com>
2022-04-08 22:48:06 +0300

    finished

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import {GlobalStyles, Wrapper} from './App.styled'
 import Footer from './components/Footer'
